325d6ecbe039bf9231cc09c7609b77988c8a65b6
RaiS4N2s <me.santosrai@gmail.com>
2020-01-28 15:47:56 +0900

    added set image functionality

--- src/BreedSelector.tsx
+++ src/BreedSelector.tsx
@@ -1,34 +1,69 @@
-import * as React from 'react'
-import { useQuery } from '@apollo/react-hooks'
-import { gql } from 'apollo-boost'
+import * as React from "react";
+import { useState } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
 
 const BREEDS = gql`
   {
-    breeds
+    dogs {
+      id
+      breed
+    }
   }
-`
+`;
+
+const GET_DOG_PHOTO = gql`
+  query Dog($breed: String!) {
+    dog(breed: $breed) {
+      id
+      displayImage
+    }
+  }
+`;
+
+interface BreedVar {
+  breed: string;
+}
 
 type BreedsQuery = {
-  breeds: string[]
+  dogs: BreedVar;
+};
+
+function DogPhoto({ breed }) {
+  const { loading, error, data } = useQuery(GET_DOG_PHOTO, {
+    variables: { breed }
+  });
+
+  if (loading) return null;
+  if (error) return `Error! ${error}`;
+
+  return (
+    <img
+      alt={data.dog.id}
+      src={data.dog.displayImage}
+      style={{ height: 100, width: 100 }}
+    />
+  );
 }
 
 export const BreedSelector = () => {
-  const { loading, error, data } = useQuery<BreedsQuery>(BREEDS)
+  const { loading, error, data } = useQuery(BREEDS);
+  const [imgSrc, setimgSrc] = useState("");
 
-  if (loading) return <div>Loading...</div>
-  if (error) return <div>Error loading the breeds :(</div>
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error loading the breeds :(</div>;
 
   return (
     <>
       <label htmlFor="breed">Choose a breed for a cute picture:</label>
-      <select name="breed" id="breed">
-        <option value="">choose U・ᴥ・U</option>
-        {data.breeds.map((breed) => (
-          <option key={breed} value={breed}>
-            {breed}
+      <select name="breed" id="breed" onChange={e => setimgSrc(e.target.value)}>
+        {data.dogs.map(dog => (
+          <option key={dog.id} value={dog.breed}>
+            {dog.breed}
           </option>
         ))}
       </select>
+      <DogPhoto breed={imgSrc} />
     </>
-  )
-}
+  );
+};
--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,6 +1,6 @@
-import ApolloClient from 'apollo-boost'
+import ApolloClient from "apollo-boost";
 
 export const apolloClient = new ApolloClient({
   // Use the uri of your own code sandbox here
-  uri: 'https://4ncfw.sse.codesandbox.io/',
-})
+  uri: "https://32ypr38l61.sse.codesandbox.io/"
+});
